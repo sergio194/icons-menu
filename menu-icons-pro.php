9d212cbcee81a1398cf8819d9e29fd0dc8005f5f
Sergio Meyniel <seriomeyniel@gmail.com>
2025-07-03 11:27:51 +0200

    Actualizar la versión del plugin Menu Icons Pro a 1.1.0 en el archivo menu-icons-pro.php.

--- menu-icons-pro.php
+++ menu-icons-pro.php
@@ -16,7 +16,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Definir constantes del plugin
-define('MENU_ICONS_PRO_VERSION', '1.0.0');
+define('MENU_ICONS_PRO_VERSION', '1.1.0');
 define('MENU_ICONS_PRO_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('MENU_ICONS_PRO_PLUGIN_PATH', plugin_dir_path(__FILE__));
 
